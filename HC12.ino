77cecf3a4a5579c60f036259d5c6f208cb62c23d
Caftree24 <105188496+Caftree24@users.noreply.github.com>
2024-01-17 21:29:00 -0600

    HC12 .ino

--- HC12.ino
+++ HC12.ino
@@ -1,26 +1,27 @@
-#define RXD2 16
-#define TXD2 17
-#define HC12 Serial2
-int pin_c =5;
-//bool signal =false;
+// CODIGO PARA CALIBRAR LAS ANTENAS EN LA MISMA FRECUENCIA
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(pin_c,OUTPUT);
-  digitalWrite(pin_c,LOW);
-  Serial.begin(115200);
-  HC12.begin(9600, SERIAL_8N1,RXD2, TXD2);
+#define RXD2 16	//(RX2)
+#define TXD2 17	//(TX2)
+#define HC12 Serial2  //Hardware serial 2 on the ESP32
 
+void setup() 
+{
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);           //Normally HIGH, LOW for settings
+  Serial.begin(115200);           // Serial port to computer
+  HC12.begin(9600, SERIAL_8N1, RXD2, TXD2);      // Serial port to HC12
 }
 
-void loop() {
-  while (HC12.available()){
-    Serial.write(HC12.read());
-    Serial.printf("Hay datos");
-    //signal = true;
+void loop() 
+{
+  while (HC12.available()) 
+  {        
+    // If HC-12 has data
+    Serial.write(HC12.read());      // Send the data to Serial monitor
   }
-  while(Serial.available()){
-    HC12.write(Serial.read());
+  while (Serial.available()) 
+  {      
+    // If we have data from Serial monitor
+    HC12.write(Serial.read());      // Send that data to HC-12
   }
-
 }
