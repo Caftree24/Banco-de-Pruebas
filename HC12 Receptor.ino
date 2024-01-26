c922565b7673ada78691d2227c799a7d622b9c58
Caftree24 <105188496+Caftree24@users.noreply.github.com>
2024-01-26 13:13:13 -0600

    Update HC12 Receptor.ino

--- HC12 Receptor.ino
+++ HC12 Receptor.ino
@@ -1,29 +1,29 @@
 
-#define LED_PIN 14
-#define RXD2 16	//(RX2)
-#define TXD2 17	//(TX2)
-#define HC12 Serial2  //Hardware serial 2 on the ESP32
+#define LED_PIN 23 //LED
 
-// ENCENDER UN LED RECIBIENDO UNA SEÑAL DE PUSH DESDE EL BOTON ATRAVEZ DE LAS HC12
 
 void setup() {
-  HC12.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
   pinMode(LED_PIN, OUTPUT);
 
 
 }
 
 void loop() {
- boolean led_state = digitalRead(LED_PIN);
+//boolean led_state = digitalRead(LED_PIN);
+ //bool var = 0;
 
-  if(HC12.available()>1){
-    int input = HC12.parseInt();
-    if(led_state == 1 && input ==1234){
+  while(Serial2.available()){
+   //bool var=Serial.write(Serial2.read());
+    if(Serial2.parseInt()==1234){
       digitalWrite(LED_PIN,HIGH);
-
+      Serial.println("Encendido");
     }
-    else if(led_state == 0 && input ==1234){
+    /*if(Serial2.parseInt()==5678){
       digitalWrite(LED_PIN,LOW);
-    }
+    }*/
   }
+  digitalWrite(LED_PIN,LOW);
 }
+      /
