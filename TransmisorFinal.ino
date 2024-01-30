9ae3e34fac44b4ea3165b72df4c4b98937ebb9d5
Caftree24 <105188496+Caftree24@users.noreply.github.com>
2024-01-30 16:08:53 -0600

    HC12TransmisorFinal.ino

--- TransmisorFinal.ino
+++ TransmisorFinal.ino
@@ -1,10 +1,14 @@
 #define B0 27
 #define B1 34
+#define B2 35
+#define B3 32
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1, 16, 17);
   pinMode(B0,INPUT);
   pinMode(B1,INPUT);
+  pinMode(B2,INPUT);
+  pinMode(B3,INPUT);
 
 
 }
@@ -12,6 +16,8 @@ void setup() {
 void loop() {
   int button_state = digitalRead(B0);
   int button_state2 = digitalRead(B1);
+  int buttoN = digitalRead(B2);
+  int buttoN2= digitalRead(B3);
   delay(500);
   Serial.println(button_state);
   //Serial.println(button_state2);
@@ -19,17 +25,29 @@ void loop() {
   if(button_state == 1){
     Serial.println("B0 presionado");
     Serial.println('1');
-    Serial2.println(123);
+    Serial2.println(122);
   }
   //while(button_state==0){ //esperan a que los botones se liberen antes de continuar.
-    if(button_state2==1){
+  if(button_state2==1){
       Serial.println("B1 presionado");
       Serial.println('2');
-      Serial2.println(456);
+      Serial2.println(344);
 
-    }
+  }
   //}
   //while(digitalRead(B1)==0){
+  if(buttoN == 1){
+    Serial.println("B2 presionado");
+    Serial.println('3');
+    Serial2.println(566);
+  }
+  //while(button_state==0){ //esperan a que los botones se liberen antes de continuar.
+  if(buttoN2==1){
+      Serial.println("B3 presionado");
+      Serial.println('4');
+      Serial2.println(788);
+
+  }
     
   //}
 
